fd9edf5dd634d51e603115ca384f8b15f66a79c0
Sunny Eyles <sunnyeyles@gmail.com>
2022-10-15 12:20:39 +0200

    Created nav-bar branch test test

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import "./styles/main.scss";
 import styled from "styled-components";
-
+import NavBar from "./components/main-page/NavBar";
+import "./functions/DataFetcher"
+import { DataFetcher } from "./functions/DataFetcher";
 function App() {
   const P = styled.p`
     color: white;
     font-size: 5rem;
     margin: 3rem;
   `;
+  // DataFetcher()
   // Keep App.jsx clear
   // Keep App.jsx clear
   // Keep App.jsx clear
   // Keep App.jsx clear
   const [count, setCount] = useState(0);
+  
 
   return (
     <div className="App">
+      <NavBar />
       <P>Mad site n dat</P>
       <P>{count}</P>
       <button onClick={() => setCount(count + 1)}>Click me harder</button>
---
+++ src/components/main-page/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar
