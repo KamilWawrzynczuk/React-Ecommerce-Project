64964f58a557069d68bbc78204ed43a50de503cb
Kamil Wawrzynczuk <kamil.wawrzynczuk@gmail.com>
2022-10-18 19:10:49 +0200

    Merge

---
+++ src/components/main-page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeCarousel from './home_components/Carousel'
+
+function Home() {
+  return (
+    <div>
+        <HomeCarousel/>
+    </div>
+  )
+}
+
+export default Home
