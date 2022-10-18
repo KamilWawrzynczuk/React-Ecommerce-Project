263e8e5c0d6f3e75f2b20052ce8d8aa8ae431268
Aaron Paul Greyling <aaron.p.greyling@gmail>
2022-10-18 13:34:24 +0200

    First Commit

--- src/components/purchases/ShoppingCart.jsx
+++ src/components/purchases/ShoppingCart.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+import styled from "styled-components";
+import { dataContext } from "../../functions/Context";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 function ShoppingCart() {
   return (
-    <div>ShoppingCart</div>
-  )
+    <Container>
+      <Header>Shopping Cart</Header>
+      <div>
+        <h3></h3>
+        <h4></h4>
+        <Button variant="outline-light">Light</Button>{" "}
+      </div>
+    </Container>
+  );
 }
 
-export default ShoppingCart
+export default ShoppingCart;
